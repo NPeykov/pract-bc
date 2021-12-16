13909185d2ec595d31f98a7af5e0bf726d995e2e
peykov <peykovn@gmail.com>
2021-12-16 19:54:29 -0300

    add filter field

--- 2/2.1/src/App.js
+++ 2/2.1/src/App.js
@@ -5,13 +5,16 @@ const INITIAL_PERSONS = [
   { name: "Tom Hendrix", phone: "011-425748" },
 ];
 
-const Persons = ({ persons }) => (
-  persons.map((person) => {
+const Persons = ({ persons, filter }) => {
+  let personsToRender = persons.filter((p) => p.name.includes(filter));
+  return personsToRender.map((person) => {
     return (
-      <p key={person.name}>{person.name} {person.phone}</p>
+      <p key={person.name}>
+        {person.name} {person.phone}
+      </p>
     );
-  })
-);
+  });
+};
 
 function nameIsTaken(persons, newName) {
   return persons.some((person) => person.name === newName.name);
@@ -19,16 +22,17 @@ function nameIsTaken(persons, newName) {
 
 const App = () => {
   const [persons, setPersons] = useState(INITIAL_PERSONS);
-  const [newPerson, setNewPerson] = useState({name: '', phone: ''});
+  const [newPerson, setNewPerson] = useState({ name: "", phone: "" });
+  const [filterText, setFilterText] = useState("");
 
   function fillNameHandler(event) {
     let actualText = event.currentTarget.value;
-    setNewPerson({...newPerson, name: actualText});
+    setNewPerson({ ...newPerson, name: actualText });
   }
 
   function fillPhoneHandler(event) {
     let actualPhone = event.currentTarget.value;
-    setNewPerson({...newPerson, phone: actualPhone});
+    setNewPerson({ ...newPerson, phone: actualPhone });
   }
 
   function addButtonHandler(event) {
@@ -38,18 +42,25 @@ const App = () => {
     } else {
       setPersons([...persons, newPerson]);
     }
-    setNewPerson({name: '', phone: ''});
+    setNewPerson({ name: "", phone: "" });
+  }
+
+  function filterPerson(event) {
+    let filterText = event.currentTarget.value;
+    setFilterText(filterText);
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <input onChange={filterPerson} />
+      <h2>Add new person</h2>
       <form>
         <div>
           name: <input onChange={fillNameHandler} value={newPerson.name} />
         </div>
         <div>
-          phone: <input onChange={fillPhoneHandler} value={newPerson.phone}/>
+          phone: <input onChange={fillPhoneHandler} value={newPerson.phone} />
         </div>
         <div>
           <button type="submit" onClick={addButtonHandler}>
@@ -58,7 +69,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <Persons persons={persons} />
+      <Persons persons={persons} filter={filterText} />
     </div>
   );
 };
