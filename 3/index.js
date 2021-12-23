0c7bf7d69525440590db219a4ef9f9fb5abde9ac
peykov <peykovn@gmail.com>
2021-12-23 04:20:35 -0300

    3

--- 3/index.js
+++ 3/index.js
@@ -1,1 +1,89 @@
-console.log("Hola mundo!")
+const express = require("express");
+const { json } = require("express/lib/response");
+const app = express();
+app.use(express.json());
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2019-05-30T17:30:31.098Z",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2019-05-30T18:39:34.091Z",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2019-05-30T19:20:14.298Z",
+    important: true,
+  },
+];
+
+const generateId = () => {
+    const maxId = notes.length > 0
+      ? Math.max(...notes.map(n => n.id))
+      : 0
+    return maxId + 1
+  }
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello world!</h1>");
+});
+
+app.get("/notes", (request, response) => {
+  response.json(notes);
+});
+
+app.get("/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => note.id === id);
+
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).json({ error: "note not found" }).end();
+  }
+});
+
+app.delete("/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).send();
+});
+
+app.post("/notes", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+      content: body.content,
+      important: body.important || false,
+      date: new Date(),
+      id: generateId()
+  }
+
+  notes = [...notes, note]
+  response.json(note)
+});
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+  
+app.use(unknownEndpoint)
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
