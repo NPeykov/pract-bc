7fa03566332456709312215136a4c140ab7430b3
peykov <peykovn@gmail.com>
2021-12-29 22:40:45 -0300

    4 refactored

--- 4/app.js
+++ 4/app.js
@@ -12,28 +12,9 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/', (request, response) => {
-    response.send('<h3>HIII</h3>')
+    response.send('<h3>Main page :)</h3>')
 })
 
-app.get('/api/blogs', blogRouter)
-
-/*
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})*/
+app.use('/api/blogs', blogRouter)
 
 module.exports = app
--- 4/routes/blog.js
+++ 4/routes/blog.js
@@ -1,13 +1,7 @@
 const blogRouter = require("express").Router();
 const controller = require("../controllers/blog");
 
-blogRouter.use((request, response, next) => {
-    console.log('ENTRE AQUI XJEJE')
-    next()
-})
-
-blogRouter.get('/api/blogs/', controller.showAllBlogs)
-
-blogRouter.post('/api/blogs/', controller.addNewBlog)
+blogRouter.get('/', controller.showAllBlogs)
+blogRouter.post('/', controller.addNewBlog)
 
 module.exports = blogRouter;
