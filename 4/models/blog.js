605cf36248142d7fd129551dd2bcf9490a67e562
peykov <peykovn@gmail.com>
2022-01-20 19:12:38 -0300

    add user schema

--- 4/models/blog.js
+++ 4/models/blog.js
@@ -4,7 +4,8 @@ const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
   url: String,
-  likes: Number
+  likes: Number,
+  author: { type: mongoose.Types.ObjectId, ref: 'User' }
 });
 
 const Blog = mongoose.model("Blog", blogSchema);
