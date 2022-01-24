bd943ce2e6d1bf3a44e7fb45d3c6b9a873ed6e2e
peykov <peykovn@gmail.com>
2022-01-23 21:17:06 -0300

    add logout logic

--- 5/src/App.js
+++ 5/src/App.js
@@ -1,44 +1,55 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import Login from './components/Login'
 import blogService from './services/blogs'
 import loginService from './services/login'
+
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [user, setUser] = useState(null)
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
   const [token, setToken] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
+      setBlogs(blogs)
     )  
   }, [])
 
-  const loginHandler = async (event) => {
-    event.preventDefault()
-    const _user = await loginService.login(username, password)
-    setUser(_user)
-    setToken(_user.token)
-    setUsername('')
-    setPassword('')
+  useEffect(() => {
+    const session = window.localStorage.getItem('userSession')
+    if(!session) {
+      return
+    }
+    const userParsed = JSON.parse(session)
+    setUser(userParsed)
+    setToken(userParsed.token)
+  }, [])
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('userSession')
+    setUser(null)
+    setToken('')
   }
 
-  return (
+  const renderBlogs = () => {
+    return (
+      <div>
+        <label>Hi {user.user_name}, you're logged in</label>
+        <button onClick={handleLogout}>Log out</button>
+        {blogs.map(blog => <Blog key={blog._id} blog={blog}/> )}
+      </div>
+    )
+  }
+
+  return (                                                                                                                                        
     <div>
       <h2>blogs</h2>
       {
-      user !== null ? 
-      blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )
+      user !== null ?
+      renderBlogs()
       :
-      <form onSubmit={loginHandler}>
-        <input type='text' placeholder='username' onChange={({ target }) => setUsername(target.value)}/>
-        <input type='text' placeholder='password' onChange={({ target }) => setPassword(target.value)}/>
-        <button>Log in</button>
-      </form> 
+      <Login setUser={setUser} setToken={setToken}/>
       }
       
     </div>
---
+++ 5/src/components/Login.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import loginService from '../services/login'
+
+const Login = ({ setUser, setToken }) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const loginHandler = async (event) => {
+    event.preventDefault()
+    const _user = await loginService.login(username, password)
+    setUser(_user)
+    setToken(_user.token)
+    window.localStorage.setItem('userSession', JSON.stringify(_user))
+    setUsername('')
+    setPassword('')
+  }
+  
+  return (
+    <>
+      <form onSubmit={loginHandler}>
+        <input type='text' placeholder='username' onChange={({ target }) => setUsername(target.value)}/>
+        <input type='text' placeholder='password' onChange={({ target }) => setPassword(target.value)}/>
+        <button>Log in</button>
+      </form> 
+    </>
+  )
+  
+}
+  
+
+export default Login
