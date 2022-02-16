7c7f201fd8666b6a236e77c1a005c9130389104e
peykov <peykovn@gmail.com>
2022-02-16 20:27:12 -0300

    6. code & some tests

---
+++ 6/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import counterReducer from './reducer/counterReducer'
+import { createStore } from 'redux'
+import ReactDOM from 'react-dom';
+
+const store = createStore(counterReducer)
+function App() {
+
+	return (
+		<div>
+			<div>
+				<label> bad: {store.getState().bad} </label>
+				<label> good: {store.getState().good} </label>
+				<label> ok: {store.getState().ok} </label>
+			</div>
+			<button onClick={() => store.dispatch({ type: 'BAD' })}> bad </button>
+			<button onClick={() => store.dispatch({ type: 'GOOD' })}> good </button>
+			<button onClick={() => store.dispatch({ type: 'OK' })}> ok </button>
+		</div>
+	);
+}
+
+const render = () => ReactDOM.render(
+	<App />,
+  document.getElementById('root')
+);
+
+render()
+
+store.subscribe(render)
+
+export default App;
