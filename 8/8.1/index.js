f623aa27a7b03b1dab64b3a007b020afef573718
peykov <peykovn@gmail.com>
2022-02-24 04:04:37 -0300

    8.1 & 8.2

--- 8/8.1/index.js
+++ 8/8.1/index.js
@@ -89,17 +89,35 @@ let books = [
 ]
 
 const typeDefs = gql`
+	type Book {
+    title: String!
+    published: Int!
+    author: String! 
+    genres: [String]!
+    id: ID!
+	}
+	
+	type Author {
+    name: String!
+    born:	Int 
+    id: ID!
+	}
+
   type Query {
 		bookCount: Int!
 		authorCount: Int!
+		allBooks: [Book]!
+		allAuthors: [Author]!
   }
 `
 
 const resolvers = {
   Query: {
 		bookCount: () => books.length,
-		authorCount: () => authors.length 
-  }
+		authorCount: () => authors.length,
+		allBooks: () => books,
+		allAuthors: () => authors 
+  },
 }
 
 const server = new ApolloServer({
